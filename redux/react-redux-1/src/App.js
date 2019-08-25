f45552e2315f3436c030ebe7ee7727105f34134e
hanbaoyi <604594231@qq.com>
2019-08-25 19:37:24 +0800

    用create-react实现react和redux功能

--- redux/react-redux-1/src/App.js
+++ redux/react-redux-1/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+	add1(){
+		this.props.onAdd1()
+	}
+	add2(){
+		this.props.onAdd2()
+	}
+	minus(){
+		this.props.onMinus()
+	}
+	addIfOdd(){
+		this.props.addIfOdd()
+	}
+	addAsync(){
+		this.props.onAddAsync()
+	}
+	render(){
+		return (
+			<div className="App">
+				你点击了 <span id="value">{this.props.value}</span> 次
+				<div>
+					<button id="add1" onClick={this.add1.bind(this)}>+1</button>
+					<button id="add1" onClick={this.add2.bind(this)}>+2</button>
+					<button id="minu1" onClick={this.minus.bind(this)}>-1</button>
+					<button id="add1IfOdd" onClick={this.addIfOdd.bind(this)}>如果是单数就+1</button>
+					<button id="addAfter2Sec" onClick={this.addAsync.bind(this)}>两秒钟后+1</button>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
--- redux/react-redux-1/src/index.js
+++ redux/react-redux-1/src/index.js
@@ -3,8 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stateChanger = (state, action) => {
+	if (state === undefined) {
+		return 0
+	} else {
+		switch (action.type) {
+			case 'add':
+				return state + action.payload // 2. 根据操作生成新的 state 触发一个事件
+			case 'minus':
+				return state - 1
+			default:
+				return state
+		}
+	}
+}
+const store = createStore(stateChanger)
+
+function add1() {
+	// 1.dispatch 一个 action
+	// 派发一个事件 === 触发一个事件
+	store.dispatch({type: 'add', payload: 1})
+}
+function add2() {
+	store.dispatch({type: 'add', payload: 2})
+}
+function onMinus() {
+	store.dispatch({type: 'minus', payload: 1})
+}
+function addIfOdd() {
+	if(store.getState() % 2 !== 0){
+		store.dispatch({type: 'add', payload: 1})
+	}
+}
+function addAsync() {
+	setTimeout(() => {
+		store.dispatch({type: 'add', payload: 1})
+	}, 2000)
+}
+const render = () => {
+	ReactDOM.render(
+		<App
+			value={store.getState()}
+			onAdd1={add1}
+			onAdd2={add2}
+			onMinus={onMinus}
+			addIfOdd={addIfOdd}
+			onAddAsync={addAsync}
+		/>,
+		document.getElementById('root')
+	);
+}
+render()
+store.subscribe(render)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
