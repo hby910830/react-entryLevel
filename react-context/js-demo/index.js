1ca9343cbfbd8cead3b05ac0ee3f1c426cc9cb91
hanbaoyi <604594231@qq.com>
2019-08-29 23:32:55 +0800

    window.setContext设置局部全局变量

--- react-context/js-demo/index.js
+++ react-context/js-demo/index.js
@@ -1,26 +1,32 @@
-let n = 100
-function f1() {
-	console.log(1);
-	f2()
-}
+{
+	let context = {}  //局部的全局变量
+	window.setContext = function (key, value) {
+		context[key] = value
+	}
 
-function f2() {
-	console.log(2);
-	f3()
-}
-n = 50
-function f3() {
-	console.log(3);
-	f4()
-}
 
-function f4() {
-	console.log(4, n)
-}
+	window.f1 = function () {
+		console.log(1);
+		f2()
+	}
 
+	function f2() {
+		console.log(2);
+		f3()
+	}
+
+	function f3() {
+		console.log(3);
+		f4()
+	}
+
+	function f4() {
+		console.log(4, context.n)
+	}
+}
+// console.log(context); 报错，无法访问，context是局部变量
+setContext('n', 20)
 setTimeout(() => {
 	f1()
 	console.log('done')
 }, 0)
-
-n = 0
