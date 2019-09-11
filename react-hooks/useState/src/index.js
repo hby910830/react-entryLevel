b7b7a3df4c11630fa5aad5972c1dd0b8775f1d49
hanbaoyi <604594231@qq.com>
2019-09-11 23:29:50 +0800

    无状态组件使用useState来代替class组件，获取状态值和方法

--- react-hooks/useState/src/index.js
+++ react-hooks/useState/src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+//用了hooks以后，react的无状态函数组件就可以不用写成class APP extends React.Component就能获取状态和属性方法
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
 function App() {
+	const [count/*值*/, setCount/*更新函数*/] = useState(10)
+	console.log(useState(10)); //[10, ƒ]
+
+	const add = () => {
+		setCount(count + 1)
+	}
+
 	return (
 		<div className='app'>
-			useState
+			<div>{count}</div>
+			<div>
+				<button onClick={add}>+1</button>
+			</div>
 		</div>
 	)
 }
