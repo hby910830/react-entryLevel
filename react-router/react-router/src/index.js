f7c7f58a3cc14220e84e0070d500e14444fc9204
hanbaoyi <604594231@qq.com>
2019-09-15 01:24:13 +0800

    上手RactRouter

--- react-router/react-router/src/index.js
+++ react-router/react-router/src/index.js
@@ -1,6 +1,6 @@
-//用了hooks以后，react的无状态函数组件就可以不用写成class APP extends React.Component就能获取状态和属性方法
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -17,63 +17,29 @@ function Box3() {
 }
 
 function App() {
-	// let hash = window.location.hash
-	let pathname = window.location.pathname
-
-	// let initUi = hash === '#signUp' ? '注册' : '登录'
-	// let initUi = pathname === '/signUp' ? '注册' : '登录'
-	let initUi = ''
-	if (pathname === '/signUp') {
-		initUi = '注册'
-	} else if (pathname === '/login') {
-		initUi = '登录'
-	} else {
-		initUi = '欢迎'
-	}
-
-	let [ui, setUi] = useState(initUi)
-	// let [ui, setUi] = useState('登录')
-
-	let onClickLogin = () => {
-		setUi('登录')
-		// window.location.hash = 'login'	 		//页面不会刷新
-		// window.location.pathname = 'login' //页面会刷新
-		window.history.pushState(null, '', 'login') //页面不会刷新,但是需要后端配合把不存在的路由引向某一个页面
-	}
-
-	let onClickSignUp = () => {
-		setUi('注册')
-		// window.location.hash = 'signUp'   //页面不会刷新
-		// window.location.pathname = 'signUp'	 //页面会刷新
-		window.history.pushState(null, '', 'signUp') //页面不会刷新,但是需要后端配合把不存在的路由引向某一个页面
-	}
-	let onClickWelcome = () => {
-		setUi('欢迎')
-		// window.location.hash = 'welcome'   //页面不会刷新
-		// window.location.pathname = 'welcome'	 //页面会刷新
-		window.history.pushState(null, '', 'welcome') //页面不会刷新,但是需要后端配合把不存在的路由引向某一个页面
-	}
-
-	let showUi = () => {
-		if (ui === '登录') {
-			return <Box1/>
-		} else if (ui === '注册') {
-			return <Box2/>
-		} else {
-			return <Box3/>
-		}
-	}
 
 	return (
 		<div className='app'>
-			<button onClick={onClickLogin}>登录</button>
-			<button onClick={onClickSignUp}>注册</button>
-			<button onClick={onClickWelcome}>欢迎</button>
-			<div>{showUi()}</div>
+			app
 		</div>
 	)
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<div>
+			<div>
+				<Link to="/"><button>首页</button></Link>
+				<Link to="/login"><button>登录</button></Link>
+				<Link to="/signUp"><button>注册</button></Link>
+				<Link to="/welcome"><button>欢迎</button></Link>
+			</div>
+			<Route path="/" exact component={App} />
+			<Route path="/login" component={Box1} />
+			<Route path="/signUp" component={Box2} />
+			<Route path="/welCome" component={Box3} />
+		</div>
+	</Router>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
