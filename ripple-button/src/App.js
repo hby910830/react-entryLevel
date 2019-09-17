00a198850212e273886537440f7b1033ccba4edd
hanbaoyi <604594231@qq.com>
2019-09-17 23:49:31 +0800

    init ripple-button

---
+++ ripple-button/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/button'
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <Button/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ripple-button/src/components/button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './button.css'
+
+export default class Button extends React.Component {
+	render() {
+		return (
+			<button className='button2'>
+				button
+				<p>button2</p>
+			</button>
+		)
+	}
+}
