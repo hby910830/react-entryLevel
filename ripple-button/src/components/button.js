62b67a38fb9707a289d1e5727fb2559145ae021c
hanbaoyi <604594231@qq.com>
2019-09-18 00:06:16 +0800

    onClick

--- ripple-button/src/components/button.js
+++ ripple-button/src/components/button.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './button.css'
 
 export default class Button extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			active: false
+		}
+	}
+
+	x() {
+		this.setState({
+			active: true
+		})
+	}
+
 	render() {
 		return (
-			<button className='button2'>
+			<button className='button2' onClick={this.x.bind(this)}>
 				{this.props.value}
+				{this.state.active ? <span>sss</span> : ''}
 			</button>
 		)
 	}
